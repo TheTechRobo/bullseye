2080d35211f60c1be8dcb2c4471040fa212e5adc
TheTechRobo <thetechrobo@proton.me>
2024-12-15 20:17:53 -0500

    test(common): add hashing test

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fs, io};
+use std::io;
 
 use base16ct::lower::encode_string;
 use sha2::{Digest, Sha256};
@@ -8,9 +8,25 @@ pub mod data;
 pub mod db;
 pub mod payloads;
 
-pub fn hash_file(mut file: fs::File) -> io::Result<String> {
+pub fn hash_file<T: io::Read>(mut file: T) -> io::Result<String> {
     let mut hasher = Sha256::new();
     io::copy(&mut file, &mut hasher)?;
     let rv: [u8; 32] = hasher.finalize().into();
     Ok(encode_string(&rv))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::hash_file;
+
+    #[test]
+    fn test_sha256() {
+        let b = "This is a STRING!\n".as_bytes();
+        let expected = "9d7780a699c93822709b3aeac17615f8bb4d2de6f17fb832a510bdf8cb96f6b9";
+        assert_eq!(
+            expected,
+            hash_file(b).unwrap(),
+        )
+    }
+}
+
